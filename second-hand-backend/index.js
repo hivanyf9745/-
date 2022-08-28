e4d3fa693beabec649990267cf67708e910fc82f
Ivan Huang <yifanhuang.Ivan@hotmail.com>
2022-08-28 12:22:12 -0400

    imported the backend feature

---
+++ second-hand-backend/index.js
@@ -0,0 +1,18 @@
+const { Pool, Client } = require('pg');
+const connectionString =
+  'postgresql://postgres:new_password@127.0.0.1:5432/second-hand';
+const pool = new Pool({
+  connectionString,
+});
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+const client = new Client({
+  connectionString,
+});
+client.connect();
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  client.end();
+});
